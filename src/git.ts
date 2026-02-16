1fecf4ed2071da76648405340e49b310dcf1f395
James Healy <james.healy@amber.com.au>
2026-02-16 13:54:23 +1100

    Always output the commit timestamp UTC
    regardless of the timezone used in the raw commit

--- src/git.ts
+++ src/git.ts
@@ -2,8 +2,9 @@ import { execSync } from 'node:child_process'
 
 export function getGitInfo(): { sha: string; timestamp: string } {
   const sha = execSync('git rev-parse HEAD', { encoding: 'utf-8' }).trim()
-  const timestamp = execSync('git log -1 --format=%cI', {
+  const rawTimestamp = execSync('git log -1 --format=%cI', {
     encoding: 'utf-8',
   }).trim()
+  const timestamp = new Date(rawTimestamp).toISOString()
   return { sha, timestamp }
 }
